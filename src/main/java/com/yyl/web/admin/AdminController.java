b593a8509bc04fd09ce7b3ed476068bb6351cdef
Yulong <2914236880@qq.com>
2022-08-02 20:54:43 +0800

    完成了“后台管理”的数据展示模块
    创建了com.yyl.web.admin包，创建了一个后台管理的控制类AdminController，用于处理前端页面请求，对应的是index.html文件

---
+++ src/main/java/com/yyl/web/admin/AdminController.java
@@ -0,0 +1,42 @@
+package com.yyl.web.admin;
+
+import com.yyl.model.ResponseData.StaticticsBo;
+import com.yyl.model.domain.Article;
+import com.yyl.model.domain.Comment;
+import com.yyl.service.ISiteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 后台管理模块
+ *
+ * 用于处理前端页面请求
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private static final Logger longger = LoggerFactory.getLogger(AdminController.class);
+
+    @Autowired
+    private ISiteService siteServiceImpl;
+    //管理中心起始页
+    @GetMapping(value = {"", "/index"})
+    public String index(HttpServletRequest request) {
+        //获取最新的5篇博客、评论以及统计数据
+        List<Article> articles = siteServiceImpl.recentArticles(5);
+        List<Comment> comments = siteServiceImpl.recentComments(5);
+        StaticticsBo staticticsBo = siteServiceImpl.getStatistics();
+        //向Request域中存储数据
+        request.setAttribute("comments", comments);
+        request.setAttribute("articles", articles);
+        request.setAttribute("statistics", staticticsBo);
+        return "back/index";
+    }
+}
